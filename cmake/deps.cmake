9eea86ce3fd06bc3658e56cdaec5244455f40d60
aoweichenn <aoweichenn@gmail.com>
2026-09-10 20:30:56 +0800

    build: bump spdlog to v1.17.0 for newer-clang consteval fix

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -11,7 +11,7 @@ FetchContent_Declare(nlohmann_json
 
 FetchContent_Declare(spdlog
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
-    GIT_TAG v1.15.0
+    GIT_TAG v1.17.0
     GIT_SHALLOW TRUE)
 
 FetchContent_Declare(Catch2
